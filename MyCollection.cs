e6506f5554b5b3ac778f280a367120f6a79bee50
SERDCEED24 <heraklakra@gmail.com>
2024-06-15 14:18:08 +0500

    Add project files.

---
+++ MyCollection.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarsLibrary;
+
+namespace MyCollectionLibrary
+{
+    public class MyCollection<T> : MyHashTable<T>, ICollection<T>, IEnumerable<T> where T : IInit, ICloneable, new()
+    {
+        // Конструкторы
+        public MyCollection() : base() { }
+
+        public MyCollection(int length) : base(length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                T item = new T();
+                item.RandomInit();
+                AddItem(item);
+            }
+        }
+
+        public MyCollection(MyCollection<T> c) : base(c.Capacity)
+        {
+            foreach (T item in c)
+            {
+                AddItem((T)item.Clone());
+            }
+        }
+
+        // Индексатор
+        public T this[T item]
+        {
+            get
+            {
+                if (item == null)
+                    throw new ArgumentNullException(nameof(item));
+                int index = FindItem(item);
+                if (index == -1)
+                    throw new InvalidOperationException("Элемент не найден в коллекции!");
+                return table[index];
+            }
+            set
+            {
+                if (item == null)
+                    throw new ArgumentNullException(nameof(item));
+                int index = FindItem(item);
+                if (index != -1)
+                {
+                    Remove(item);
+                    Add(value);
+                }
+                else
+                    throw new InvalidOperationException("Элемент не найден в коллекции!");
+            }
+        }
+
+        // Свойства
+        public bool IsReadOnly => false;
+
+        // Методы
+        public void Add(T item)
+        {
+            AddItem(item);
+        }
+        public void Clear()
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                table[i] = default;
+                wasDeleted[i] = false;
+            }
+            count = 0;
+        }
+        public bool Contains(T item)
+        {
+            return base.Contains(item);
+        }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (array.Length - arrayIndex < count)
+                throw new ArgumentException("В конечном массиве меньше элементов чем в исходной коллекции!");
+
+            int j = arrayIndex;
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (table[i] != null)
+                {
+                    array[j++] = table[i];
+                }
+            }
+        }
+        public bool Remove(T item)
+        {
+            return RemoveData(item);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (table[i] != null)
+                {
+                    yield return table[i];
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ MyHashTable.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarsLibrary;
+
+namespace MyCollectionLibrary
+{
+    public class MyHashTable<T> where T : IInit, ICloneable, new()
+    {
+        // Поля
+        protected T[] table;
+        protected bool[] wasDeleted;
+        protected int count = 0;
+        protected double fillRatio;
+
+        // Свойства
+        public int Capacity => table.Length;
+        public int Count => count;
+        public T[] Table => table;
+
+        // Конструктор
+        public MyHashTable(int size = 10, double fillRatio = 0.72)
+        {
+            table = new T[size];
+            wasDeleted = new bool[size];
+            this.fillRatio = fillRatio;
+        }
+
+        //Методы
+        public bool Contains(T data)
+        {
+            return !(FindItem(data) < 0);
+        }
+        public bool RemoveData(T data)
+        {
+            int index = FindItem(data);
+            if (index < 0)
+                return false;
+            count--;
+            table[index] = default;
+            wasDeleted[index] = true;
+            return true;
+        }
+        public void Print()
+        {
+            int i = 0;
+            foreach (T item in table)
+            {
+                Console.WriteLine($"{i}: {item}");
+                i++;
+            }
+        }
+        public void AddItem(T item)
+        {
+            if ((double)Count / Capacity > fillRatio)
+            {
+                T[] tempTable = (T[])table.Clone();
+                table = new T[tempTable.Length * 2];
+                bool[] tempWD = (bool[])wasDeleted.Clone();
+                wasDeleted = new bool[tempWD.Length * 2];
+                count = 0;
+                for (int i = 0; i < tempTable.Length; i++)
+                {
+                    AddData(tempTable[i]);
+                    wasDeleted[i] = tempWD[i];
+                }
+            }
+            AddData(item);
+        }
+        private int GetIndex(T data)
+        {
+            return Math.Abs(data.GetHashCode()) % Capacity;
+        }
+        public void AddData(T data)
+        {
+            if (data == null)
+                return;
+            int index = GetIndex(data);
+            int current = index;
+            if (table[index] != null)
+            {
+                while (current < table.Length && table[current] != null)
+                    current++;
+                if (current == table.Length)
+                {
+                    current = 0;
+                    while (current < index && table[current] != null)
+                        current++;
+                    if (current == index)
+                    {
+                        throw new Exception("Нет места в таблице!");
+                    }
+                }
+            }
+            table[current] = data;
+            count++;
+        }
+        public int FindItem(T data)
+        {
+            int index = GetIndex(data);
+            if (table[index] == null && !wasDeleted[index])
+                return -1;
+            if (table[index] != null && table[index].Equals(data))
+                return index;
+            else
+            {
+                int current = index;
+                while (current < table.Length)
+                {
+                    if (table[current] != null && table[current].Equals(data))
+                        return current;
+                    current++;
+                }
+                current = 0;
+                while (current < index)
+                {
+                    if (table[current] != null && table[current].Equals(data))
+                        return current;
+                    current++;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
